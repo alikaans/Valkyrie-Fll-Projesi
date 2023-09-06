5c34576a6d3c62533ce755c7f85fd5e4a8ff720d
HerrLavrencium <patatesking91@gmail.com>
2023-09-06 21:12:30 +0300

    unity level1

---
+++ test-frc/Assets/Scripts/kazandin.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kazandin : MonoBehaviour
+{
+	
+	public GameObject Kazandin;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        Kazandin.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			Kazandin.SetActive(true);
+		}
+	}
+}
